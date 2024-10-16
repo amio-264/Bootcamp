21926fbf4d400f3e38611653dca1bc7e9f29aebb
amio-264 <amiosarkar264@gmail.com>
2024-10-17 02:27:13 +0600

    c++

---
+++ data_type.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // int a;
+    // float b;
+    // cout << "Input two number: ";
+    // cin >> a >> b; //Taking input from user
+    // cout << a << "\n" << b;
+
+    // int x = 10000000000; //limitations of data type
+    // cout << x;
+
+    // int = 4 bytes
+    // long long int = 8 bytes
+    // float = 4 bytes
+    // double = 8 bytes
+    // char = 1 byte
+
+    cout << sizeof(int);
+    cout << sizeof(float);
+    return 0;
+}
---
+++ hello.cpp
@@ -0,0 +1,6 @@
+// header file
+#include<iostream>
+int main(){
+    std::cout << "Hello World";
+    return 0;
+}
