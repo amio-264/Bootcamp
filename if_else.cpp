2607404dda6d7192cb8bdc050ef875dcb6a79f81
amio-264 <amiosarkar264@gmail.com>
2024-10-17 03:33:32 +0600

    All about if else

--- if_else.cpp
+++ if_else.cpp
@@ -1,14 +1,38 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
     int price;
     cout << "Enter price: ";
     cin >> price;
-    if(price>=150){
-        cout << "Burger";
+    if (price >= 300)
+    {
+        if (price >= 500)
+        {
+            cout << "Burger , Fries , Coke and Ice Cream";
+        }
+        else
+        {
+            cout << "Only Burger";
+        }
     }
-    else{
+    else if (price >= 100)
+    {
+        if (price >= 200)
+        {
+            cout << "Fries and Ice Cream";
+        }
+        else if (price >= 150)
+        {
+            cout << "Fries and Coke";
+        }
+        else
+        {
+            cout << "Only Fries";
+        }
+    }
+    else
+    {
         cout << "No Burger";
     }
     return 0;
