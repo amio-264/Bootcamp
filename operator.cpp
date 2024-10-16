5fb3af717766f0a1696db28851ea48f9961a0872
amio-264 <amiosarkar264@gmail.com>
2024-10-17 03:01:30 +0600

    Operator

---
+++ operator.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x = 5;
+    // Arithmetic Operators ( + , - , * , - , / , % )
+    cout << 10 + 5 << " " << 23 % 2 << " " << 12 - 2 << " " << 2 * 6 << " " << 34 / 2 << " ";
+    // Assignment Operators
+    x += 1; // x = x + 1
+    cout << x;
+    // Relational Operators ( < , > , <= , >= , == , != )
+    // Logical Operators ( && , || )
+}
