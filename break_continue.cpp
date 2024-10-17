6a584e32ad39b9718218c28abde10f1a776f00bb
amio-264 <amiosarkar264@gmail.com>
2024-10-18 01:56:41 +0600

    Usage of break and continue

---
+++ break_continue.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    for (int i = 1; i <= 10; i++)
+    {
+        if (i == 5)
+        {
+            break;
+        }
+        cout << i << " ";
+    }
+
+    cout << "\n";
+
+    for (int i = 1; i <= 10; i++)
+    {
+        if (i == 5)
+        {
+            continue;
+        }
+        cout << i << " ";
+    }
+    return 0;
+}
