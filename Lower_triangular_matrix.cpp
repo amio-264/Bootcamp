124744ad0b7053a26499812b43d72390fd7f2bea
amio-264 <amiosarkar264@gmail.com>
2024-10-31 03:35:32 +0600

    Lower Triangular matrix

---
+++ Lower_triangular_matrix.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int m[n + 1][n + 1];
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            cin >> m[i][j];
+        }
+    }
+    bool mat = true;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            if (i >= j)
+            {
+                if (m[i][j] >= 1)
+                {
+                }
+                else
+                {
+                    mat = false;
+                }
+            }
+            else
+            {
+                if (m[i][j] == 0)
+                {
+                }
+                else
+                {
+                    mat = false;
+                }
+            }
+        }
+    }
+    if (mat)
+    {
+        cout << "Yes" << endl;
+    }
+    else
+    {
+        cout << "No";
+    }
+    return 0;
+}
