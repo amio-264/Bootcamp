07d9cc55d3895a10b027c8648601df259190b8b5
amio-264 <amiosarkar264@gmail.com>
2024-11-02 23:37:08 +0600

    string

---
+++ string.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    cin>>s;
+    int length = s.size();
+    cout << length << endl;
+    cout << s << endl;
+    return 0;
+}
